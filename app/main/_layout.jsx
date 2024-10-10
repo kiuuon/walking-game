cf7e9dcfcf9c80298dc17e47431a3ea8cbef1e58
kiuuon <k41471804@gmail.com>
2024-10-10 23:27:55 +0900

    feat: 메인 페이지 레이아웃 구성

--- app/main/_layout.jsx
+++ app/main/_layout.jsx
@@ -1,15 +1,17 @@
 import { Stack } from "expo-router";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  header: { height: 100, backgroundColor: "#D9D9D9" },
+  main: { height: 400 },
+});
 
 export default function MainLayout() {
   return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          height: 500,
-        }}
-      ></View>
-
+    <View style={styles.container}>
+      <View style={styles.header} />
+      <View style={styles.main} />
       <Stack screenOptions={{ headerShown: false }} />
     </View>
   );
